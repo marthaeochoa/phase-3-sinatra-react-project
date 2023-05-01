733990e1cd45d7610eb8b9661d0a80eb0046bc63
Martha Ochoa <marthaeochoa98@gmail.com>
2023-04-30 22:00:19 -0500

    adding time stamps

--- db/migrate/20230501023432_create_comments.rb
+++ db/migrate/20230501023432_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :comment
       t.integer :post_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_022654) do
+ActiveRecord::Schema.define(version: 2023_05_01_023432) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
