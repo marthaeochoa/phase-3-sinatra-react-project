0d2527d1820fd53beff0f96355d45491600d7fbc
Martha Ochoa <marthaeochoa98@gmail.com>
2023-04-30 21:39:18 -0500

    create migrations and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_014433) do
+ActiveRecord::Schema.define(version: 2023_05_01_022654) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "blog_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
