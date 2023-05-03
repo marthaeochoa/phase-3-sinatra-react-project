2944f16076611f0f8bebaa184c69d8667a195dca
Martha Ochoa <marthaeochoa98@gmail.com>
2023-05-03 17:04:07 -0500

    first seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+10.times do 
+    blog = Blog.create(
+        title: Faker::Lorem.sentence,
+        description: Faker::Lorem.paragraph
+    )
+end 
 
 puts "✅ Done seeding!"
