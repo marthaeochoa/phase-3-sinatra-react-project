1170e2f1fe655043db140cf542371a7df24b2bb2
Martha Ochoa <marthaeochoa98@gmail.com>
2023-05-03 17:18:45 -0500

    second seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,20 @@ puts "🌱 Seeding spices..."
 10.times do 
     blog = Blog.create(
         title: Faker::Lorem.sentence,
-        description: Faker::Lorem.paragraph
+        description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true)
     )
-end 
+
+
+    rand(1..5).times do
+        post = Post.create(
+        title: Faker::Lorem.sentence,
+        description: Faker::Lorem.paragraph(sentence_count: 4, supplemental: true),
+        blog_id: blog.id
+        )
+      end
+end
+
+
+
 
 puts "✅ Done seeding!"
