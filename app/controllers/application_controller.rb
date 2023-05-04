c6cdef0f79a638792a1f93121bf0b1bda0025ac8
Martha Ochoa <marthaeochoa98@gmail.com>
2023-05-04 18:16:31 -0500

    add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,42 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/blogs" do
+    blogs = Blog.all
+    blogs.to_json
+  end
+
+  get "/blogs/:id" do
+    blog = Blog.find(params[:id])
+    blog.to_json(include: :posts)
+  end 
+
+  post 'blogs' do 
+    blog = Blog.create(
+      title: params[:title],
+      description: params[:description]
+    )
+    blog.to_json
+  end 
+
+  patch '/blogs/:id' do
+    blog = Blog.find(params[:id])
+    blog.update(
+      name: params[:name], 
+      description: params[:description]
+      )
+    blog.to_json
+  end
+
+  delete '/blogs/:id' do 
+    blog = Blog.find(params[:id])
+    blog.destroy
+    blog.to_json
+  end
+
+  get '/posts' do
+    posts = Post.all 
+    posts.to_json
   end
 
 end
